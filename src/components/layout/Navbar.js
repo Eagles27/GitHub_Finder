ab30db7a4fe38550ca3122842b8c5d91259ecf53
Eagles27 <92862523+Eagles27@users.noreply.github.com>
2022-05-25 10:54:06 +0200

    Adding Routing + About Page

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { AiFillGithub } from "react-icons/ai";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Navbar = (props) => {
   return (
@@ -8,6 +9,14 @@ const Navbar = (props) => {
       <h1>
         <AiFillGithub /> {props.title}
       </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
