e45dd1790b556d2312fe70485ec239dc7bc0623b
Eagles27 <92862523+Eagles27@users.noreply.github.com>
2022-05-26 21:16:45 +0200

    Correction to link to user Profile + Refactoring User/Users/Loading components to uses Hooks

--- src/App.js
+++ src/App.js
@@ -1,72 +1,71 @@
-import React, { Component, Fragment } from "react";
+import React, { useState, Fragment } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import axios from "axios";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
+import User from "./components/users/User";
 
-class App extends Component {
-  state = {
-    users: [],
-    loading: false,
-    alert: null,
-  };
+import GithubState from "./context/github/GithubState";
 
-  /// Search GitHub User
-  searchUsers = async (text) => {
-    this.setState({ loading: true });
+import "./App.css";
+
+const App = () => {
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState(null);
+
+  ///Get Users Repos
+
+  const getUserRepos = async (username) => {
+    setLoading(true);
+
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-
-    this.setState({ users: res.data.items, loading: false });
+    setRepos(res.data);
+    setLoading(false);
   };
-
-  /// Clear Users
-  clearUsers = async () => this.setState({ users: [], loading: false });
 
   /// Set Alert
-  setAlert = (msg, type) => {
-    this.setState({ alert: { msg, type } });
 
-    setTimeout(() => this.setState({ alert: null }), 5000);
+  const showAlert = (msg, type) => {
+    setAlert({ msg, type });
+    setTimeout(() => setAlert(null), 5000);
   };
 
-  render() {
-    const { users, loading } = this.state;
-
-    return (
+  return (
+    <GithubState>
       <Router>
         <div className="App">
           <Navbar />
           <div className="container">
-            <Alert alert={this.state.alert} />
+            <Alert alert={alert} />
             <Routes>
               <Route
                 exact
                 path="/"
                 element={
                   <Fragment>
-                    <Search
-                      searchUsers={this.searchUsers}
-                      clearUsers={this.clearUsers}
-                      showClear={users.length > 0 ? true : false}
-                      setAlert={this.setAlert}
-                    />
-                    <Users loading={loading} users={users} />
+                    <Search setAlert={showAlert} />
+                    <Users />
                   </Fragment>
                 }
               />
               <Route exact path="/about" element={<About />} />
+              <Route
+                exact
+                path="/user/:login"
+                element={<User getUserRepos={getUserRepos} repos={repos} />}
+              />
             </Routes>
           </div>
         </div>
       </Router>
-    );
-  }
-}
+    </GithubState>
+  );
+};
 
 export default App;
